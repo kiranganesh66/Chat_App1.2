1781bd2bc5003d96aa72e3f526a4a9d2c6428ead
kiran66 <kiranganesh66@gmail.com>
2024-10-30 17:15:17 +0530

    addingnewoptions

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ require("dotenv").config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // allow specific origin
+  
+ origin: "https://deploy-mern-1whq.vercel.app", // allow specific origin
+//  origin: "http://localhost:5173",
   methods: ["GET", "POST"], // specify allowed methods
   credentials: true, // enable cookies for cross-origin requests
 };
